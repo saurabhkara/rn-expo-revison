98cadb426c7c006610bf1a227312d54cc73fcd6d
wovvmac <wovvmac@wovvmacs-MacBook-Pro.local>
2025-05-07 12:09:14 +0530

    expo navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack
+    screenOptions={
+      {
+        headerStyle: {
+          backgroundColor: 'orange'
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: 'bold'
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: 'white'
+        }
+      }
+    }
+  >
+    <Stack.Screen
+      name="index" options={{ title: 'Home' }} />
+  </Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet,Image } from "react-native";
+import notes from '@/assets/images/note.png'
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={notes} style={styles.image} />
+      <Text>Welcome to Note App</Text>
+      <Text></Text>
     </View>
   );
 }
+const styles= StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image:{
+    height:100,
+    width:100,
+    tintColor:'orange',
+
+  }
+})
